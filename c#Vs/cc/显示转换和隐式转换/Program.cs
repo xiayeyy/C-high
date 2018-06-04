eea896889fef3183390d7784fa8a09295fc1f63b
xiayeyy <xiayeyy@yeah.net.com>
2018-06-04 20:56:25 +0800

    5.4

--- c#Vs/cc/显示转换和隐式转换/Program.cs
+++ c#Vs/cc/显示转换和隐式转换/Program.cs
@@ -13,6 +13,10 @@ namespace 显示转换和隐式转换
             byte mybyte = 155;  //byte类型取值在0-255
             int myint = mybyte;  //int 范围大，编译器自动进行隐式转换 
             mybyte = (byte)myint;//强制转换
+
+            string mystring = "123";
+            myint = Convert.ToInt32(mystring);
+
         }
     }
 }
